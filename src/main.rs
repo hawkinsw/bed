2be924dc318dfe9efcbcc8c4d83c1e195c62a946
Will Hawkins <hawkinsw@borlaugic.com>
2018-08-27 14:54:55 -0400

    Write output to stdout.

--- src/main.rs
+++ src/main.rs
@@ -3,77 +3,78 @@ use std::u8;
 use std::u64;
 use std::env;
 use std::io::prelude::*;
-use std::fs::File;
+//use std::fs::File;
 
 fn read_file<T: Read>(fd: &mut T) -> io::Result<Vec<u8>> {
 	let mut contents = Vec::<u8>::new();
 
 	match fd.read_to_end(&mut contents) {
-		Ok(s) => {
-			println!("Read {} bytes.", s);
+		Ok(_) => {
 			Ok(contents)
 		}
 		Err(e) => {
-			println!("Read failure: {}", e);
 			Err(e)
 		}
 	}
 }
 
-fn print_usage() {
+fn write_file<T: Write>(fd: &mut T, output: &[u8]) -> io::Result<()> {
+	fd.write_all(output)
+}
+
+fn print_usage(program_name: String) {
+	println!("{} <replacement index> <replacement byte 1> ... <replacement byte n>", program_name);
 }
 
 fn main() -> std::io::Result<()> {
 	let mut args: Vec<String> = env::args().collect();
 	let replace: u64;
 	let mut replacement: Vec<u8> = Vec::<u8>::new();
+	let mut input: io::Stdin = io::stdin();
+	let mut output: io::Stdout = io::stdout();
+	let mut contents: Vec<u8>;
 
 	if args.len() <= 2 {
-		print_usage();
+		print_usage(args[0].clone());
 		return Err(io::Error::new(io::ErrorKind::Other,
 		                          "Not enough parameters."));
 	}
 
 	replace = match u64::from_str_radix(&args[1], 16) {
 		Ok(v) => v,
-		Err(e) => return Err(io::Error::new(io::ErrorKind::Other,
-		                          "Error parsing parameter."))
+		Err(_) => return Err(io::Error::new(io::ErrorKind::Other,
+		                          "Error parsing replacement index parameter."))
 	};
 
+	/*
+	  From now on we are only going to consider the replacement
+		content parameters.
+	 */
 	args = args[2..].to_vec();
 
-	for r in args.iter() {
+	for (i, r) in args.iter().enumerate() {
 		let rv: u8 = match u8::from_str_radix(r, 16) {
 			Ok(v) => v,
-			Err(e) => return Err(io::Error::new(io::ErrorKind::Other,
-		                          "Error parsing parameter."))
+			Err(_) => return Err(io::Error::new(io::ErrorKind::Other,
+		                       format!("Error parsing {}th replacement content parameter.", i+1)))
 		};
-		println!("Pushing {}", rv);
 		replacement.push(rv);
 	}
 
-	println!("Replace {} with ...", replace);
-	let mut fd = io::stdin();
-	let mut contents = read_file(&mut fd).unwrap();
+	contents = read_file(&mut input).unwrap();
 
-	println!("Old:");
-	for i in contents.iter() {
-		println!("{}", i);
-	}
+	/*
+	  TODO: Make sure that the replacement fits within the content.
+	 */
 
 	for i in (replace .. replace + (replacement.len() as u64)) {
 		contents[i as usize] = replacement.pop().unwrap();
-		println!("i: {}", i);
 	}
 
-	println!("New:");
-	for i in contents.iter() {
-		println!("{}", i);
-	}
+	write_file(&mut output, &contents)
 
 	/*
-	  Closing _fd_ is not necessary because io::File's are
-		implicitly closed when they go out of scope.
+	  Closing input and output is not necessary because io::File's 
+		are implicitly closed when they go out of scope.
 	 */
-	Ok(())
 }
